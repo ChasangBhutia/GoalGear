960ca5a555453cbce9d311915709ec3fe86c0514
ChasangBhutia <chasangtsering@gmail.com>
2025-07-17 11:53:54 +0530

    prod: testing production

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const upload = require('../config/multer');
 const { getAllProducts, getProduct, createProduct, deleteProduct } = require('../controllers/productControllers');
-// const {isLoggedIn, isAdmin} = require('../middleware/isLoggedIn');
+const {isLoggedIn, isAdmin} = require('../middleware/isLoggedIn');
 const router = express.Router();
 
-router.get('/', getAllProducts);
-router.get('/:productId', getProduct);
-router.post('/', upload.single('image'), createProduct);
-router.delete('/:productId', deleteProduct);
+router.get('/',isLoggedIn, getAllProducts);
+router.get('/:productId', isLoggedIn, getProduct);
+router.post('/', isLoggedIn, isAdmin, upload.single('image'), createProduct);
+router.delete('/:productId', isLoggedIn, isAdmin, deleteProduct);
 
 
 module.exports = router;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { uploadProfileImage,  addAddress, deleteAddress } = require('../controllers/userControllers');
 const { getUser } = require('../controllers/authControllers');
 const { addToCart, removeFromCart, getCart } = require('../controllers/cartControllers');
-// const {isLoggedIn, isUser} = require('../middleware/isLoggedIn');
+const {isLoggedIn, isUser} = require('../middleware/isLoggedIn');
 const router = express.Router();
 const upload = require('../config/multer');
 
@@ -10,15 +10,15 @@ router.get('/', (req, res)=>{
     res.send("Its working");
 })
 
-router.get('/profile', getUser);
-router.put('/profile/image',upload.single('image'), uploadProfileImage)
-router.post('/profile/address', addAddress);
-router.delete('/profile/address/:addressId', deleteAddress);
+router.get('/profile',isLoggedIn, getUser);
+router.put('/profile/image',upload.single('image'), isLoggedIn, isUser, uploadProfileImage)
+router.post('/profile/address', isLoggedIn, isUser, addAddress);
+router.delete('/profile/address/:addressId', isLoggedIn, isUser, deleteAddress);
 
 // Cart APIs
-router.get('/cart', getCart);
-router.post('/cart/:productId', addToCart)
-router.delete('/cart/:productId', removeFromCart)
+router.get('/cart', isLoggedIn, isUser, getCart);
+router.post('/cart/:productId',isLoggedIn, isUser, addToCart)
+router.delete('/cart/:productId',isLoggedIn, isUser, removeFromCart)
 
 
 module.exports = router;
